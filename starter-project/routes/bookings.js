dfd37d636bfc1529d0a18df61680c41b0a32398b
polgasull <pol89.gn@gmail.com>
2017-08-13 13:34:05 +0200

    Facebook & google plus login crashed, but the route until booking success works without take in account dates. Anyway is creating the booking database :)

--- starter-project/routes/bookings.js
+++ starter-project/routes/bookings.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const Booking = require("../models/booking");
 const Room = require("../models/room");
-
+const User = require("../models/user");
 
 router.post('/', (req, res, next) => {
     const checkInDate = req.body.checkInDate;
@@ -11,18 +11,38 @@ router.post('/', (req, res, next) => {
     const nightDate = {"nights.date": new Date()};
     const nightBooked = {"nights.booked": false};
 
-    Room.find({"nights.date": checkInDate, "nights.booked": false}, (err, rooms) => {
+    Room.find({"nights.date": checkInDate, "nights.booked": false}, (err, bookings) => {
         if (err) {
             next(err);
         }
         else {
-            res.render('bookings/index', { rooms });
+            res.render('bookings/index', { bookings });
         }   
     })
 })
 
-
-
-
+router.get('/new', (req, res, next) => {
+    res.render('bookings/new');
+  })
+  
+router.post('/success', (req, res, next) => {
+    const bookingInfo = {
+      checkInDate: req.body.checkInDate,
+      checkOutDate: req.body.checkOutDate, 
+      name: req.body.name,
+      surname: req.body.surname,
+      country: req.body.country,
+      email: req.body.email,
+    };
+  
+    const newBooking = new Booking(bookingInfo);
+    newBooking.save( (err) => {
+        if (err) { 
+            return next(err) 
+        } else {
+            res.render('bookings/success');
+        }
+    });
+});
 
 module.exports = router;
